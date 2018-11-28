303eb2aaa18515e7cf7d91e22e8bcdc006b69feb
Ivan BARAN <ibaran@e1r9p20.42.fr>
2018-11-28 18:34:44 +0100

    no comments (my repo, i do what i want)

--- tester.sh
+++ tester.sh
@@ -1,8 +1,15 @@
 valgrind ./test_gnl ./britney
+sleep 5
 valgrind ./test_gnl ./empty
+sleep 5
 valgrind ./test_gnl ./line8_with_n
+sleep 5
 valgrind ./test_gnl ./line8_no_n
+sleep 5
 valgrind ./test_gnl ./line16_with_n
+sleep 5
 valgrind ./test_gnl ./line16_no_n
+sleep 5
 valgrind ./test_gnl ./random
+sleep 5
 valgrind ./test_gnl ./Makefile
